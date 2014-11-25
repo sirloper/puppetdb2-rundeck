d16378c5466761afde37152feb8a556ece9d0fe0
Mark Cartwright <sirloper@gmail.com>
2014-11-25 13:10:53 -0500

    Revert SuperMicro Hack
    The hack made the catalog show correctly, but upon execution it would throw a java error about not being able to conenc to the host.  Anothe rpatch forthcoming when this is tracked down.

--- puppetdb-rundeck.rb
+++ puppetdb-rundeck.rb
@@ -54,12 +54,7 @@ get '/' do
 	host     = d['certname']
 	name     = d['name'] if d['name'] != "hostname"
         value    = d['value'] if d['name'] != "hostname"
-	# Hack that will hide serial numbers but prevent supermicro macines from killing the parse to runeck.
-	if d['name'] = 'serialnumber'
-		 rundeck_resources[host][name] = 'Not Specified'
-	else
-		rundeck_resources[host][name] = value
-	end
+	rundeck_resources[host][name] = value
 	}
 
 	rundeck_resources.to_yaml
